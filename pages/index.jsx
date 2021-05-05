c205f1c823e5ce4db39376d50fbe3e0ab4ff7ece
Dmitrii <dyakovlev@freetechs.org>
2021-05-05 23:01:21 +0300

    meta data

--- pages/index.jsx
+++ pages/index.jsx
@@ -35,13 +35,27 @@ const Index = () => {
         <title>All IN - barbers and more</title>
         <meta
           name="description"
-          content="All IN - это не просто барбершоп.Это место, в котором мы собрали профессионалов с многолетним опытом работы и им доверяем.Мы создали пространство где можно не думать о результате, потому что он всегда будет предвосхищать, ведь за вашу голову мы отвечаем своей."
+          content='All IN - это не просто барбершоп.Это место, в котором мы собрали профессионалов с многолетним опытом работы и им доверяем.Мы создали пространство где можно не думать о результате, потому что он всегда будет предвосхищать, ведь за вашу голову мы отвечаем своей.'
         />
         <link rel="icon" href="/header/all-in-logo.svg" />
+        <link rel='canonical' href='https://all-in-barbers.com' />
         <meta name='viewport' content='width=device-width,initial-scale=1:2'/>
         <meta content='true' name='HandheldFriendly'/>
         <meta content='width' name='MobileOptimized'/>
         <meta content='yes' name='apple-mobile-web-app-capable'/>
+        <meta property="twitter:title" content='All IN - barbers and more' />
+        <meta property="twitter:url" content='https://all-in-barbers.com' />
+        <meta property="twitter:creator" content='https://all-in-barbers.com' />
+        <meta property="twitter:description" content='All IN - это не просто барбершоп.Это место, в котором мы собрали профессионалов с многолетним опытом работы и им доверяем.Мы создали пространство где можно не думать о результате, потому что он всегда будет предвосхищать, ведь за вашу голову мы отвечаем своей.' />
+        <meta property="twitter:image" content='/header/all-in-logo.svg' />
+        <meta property="twitter:site" content='https://all-in-barbers.com' />
+
+        <meta property="og:type" content='product' />
+        <meta property="og:url" content='https://all-in-barbers.com' />
+        <meta property="og:title" content='All IN - barbers and more' />
+        <meta property="og:image" content='/header/all-in-logo.svg' />
+        <meta property="og:description" content='All IN - это не просто барбершоп.Это место, в котором мы собрали профессионалов с многолетним опытом работы и им доверяем.Мы создали пространство где можно не думать о результате, потому что он всегда будет предвосхищать, ведь за вашу голову мы отвечаем своей.' />
+        <meta property="og:site_name" content='https://all-in-barbers.com' />
       </Head>
       <div className={styles.pageContainer}>
         <div className={styles.pageWrapper}>
